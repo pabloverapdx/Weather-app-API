3053d2519a22e2b69f160f60436800e75a8a20f5
Diane Douglas <diane.douglas1@gmail.com>
2016-02-12 14:36:23 -0800

    Add browserify task in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('jsBrowserify', function(){
+  return browserify({ entries: ['./js/browser.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
